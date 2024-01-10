da56fa4bd32003f578363755d4f9e866eef7af23
pedromourax <pedrolucas.moura@hotmail.com>
2024-01-10 20:40:57 -0300

    removed trash files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CursosController } from './cursos/cursos.controller';
 import { BookModule } from './book/book.module';
 
 @Module({
   imports: [BookModule],
-  controllers: [AppController, CursosController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
